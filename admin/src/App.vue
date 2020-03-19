e936bdafb7f9021c8aae4678fc7944ab46d7ab55
bill080307 <bill080307@live.cn>
2020-03-19 16:43:05 +0800

    管理界面更新

---
+++ admin/src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col>ipfs API: {{ipfsApi}}</b-col>
+        <b-col>key:
+          <b-button variant="info" @click="init">refresh</b-button>
+          <b-button variant="success" @click="newkey">new</b-button>
+          <b-form-select v-model="keyselected" :options="keys" @change="selectkey"></b-form-select>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="3">
+          <b-form-select v-model="updateed" :options="updates" :select-size="10" @change="selectver"></b-form-select>
+        </b-col>
+        <b-col cols="9">
+          <b-form-group label="title:" label-for="title">
+            <b-form-input id="title" v-model="item.title"></b-form-input>
+          </b-form-group>
+          <b-form-group label="version:" label-for="version">
+            <b-form-input id="version" v-model="item.version"></b-form-input>
+          </b-form-group>
+          <b-form-group label="bulid:" label-for="bulid">
+            <b-form-input id="bulid" v-model="item.bulid"></b-form-input>
+          </b-form-group>
+          <b-form-group label="update_log:" label-for="update_log">
+            <b-form-textarea
+                    id="update_log"
+                    v-model="item.log"
+                    rows="3"
+                    max-rows="6"
+            ></b-form-textarea>
+          </b-form-group>
+          <b-form-group label="apkfile:" label-for="apkfile">
+            <b-form-file id="apkfile" v-model="apkfile" accept=".apk"></b-form-file>
+          </b-form-group>
+          <p>download: {{item.apk_file}}</p>
+          <p>datetime: {{ item.datetime | dateT}}</p>
+          <b-button-group>
+            <b-button variant="success" @click="newversion">new</b-button>
+            <b-button variant="info">update</b-button>
+            <b-button variant="warning">delete</b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import Axios from 'axios'
+  export default {
+    name: 'App',
+    data(){
+      return {
+        ipfsApi:"",
+        keys:[],
+        keyselected:"",
+        item:{
+          "title": "",
+          "version": "",
+          "bulid": "",
+          "apk_file": "",
+          "log": "",
+          "datetime": "",
+        },
+        updatejson:[],
+        updates:[],
+        updateed:"",
+        apkfile:null
+      }
+    },
+    methods:{
+      init(){
+        Axios.get('/api/getkeys').then((res)=>{
+          this.ipfsApi = res.data.api;
+          this.keys = [];
+          for (let i = 0; i < res.data.keys.length; i++) {
+            this.keys.push({
+              value: res.data.keys[i].Id,
+              text: res.data.keys[i].Name
+            })
+          }
+        });
+      },
+      newkey(){
+        let name = prompt("input key name","");
+        if(!name)return;
+        Axios.get('/api/newkey?keyname='+name).then((res)=>{
+          console.log(res);
+          this.init()
+        })
+      },
+      selectkey(){
+        Axios.get('/api/getupdate?ipns='+this.keyselected).then((res)=>{
+          this.updatejson = res.data.data;
+          this.updates = [];
+          for (let i = 0; i < this.updatejson.length; i++) {
+            this.updates.push({
+              value: i,
+              text: this.updatejson[i].title
+            })
+          }
+        })
+      },
+      selectver(){
+        this.item = this.updatejson[this.updateed];
+      },
+      newversion(){
+        const formdata = new FormData();
+        formdata.append('apk',this.apkfile);
+        formdata.append('ipns',this.keyselected);
+        formdata.append('title',this.item.title);
+        formdata.append('version',this.item.version);
+        formdata.append('bulid',this.item.bulid);
+        formdata.append('log',this.item.log);
+        Axios.post('/api/newversion',formdata,{
+          headers:{'Content-Type':'multipart/form-data'}
+        }).then((res)=>{
+          this.item = {};
+          this.apkfile = null;
+          this.selectkey();
+          console.log(res)
+        })
+      }
+    },
+    filters:{
+      dateT(timestamp){
+        return new Date(timestamp * 1000).toLocaleString()
+      }
+    },
+    created(){
+      this.init();
+      console.log()
+    }
+  }
+</script>
+
+<style>
+</style>
